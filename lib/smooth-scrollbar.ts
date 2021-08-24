3d2cee1d9561c9f3b6ed1535e5b8920dce88b36c
nghiepit <me@nghiepit.dev>
2021-08-24 21:33:22 +0700

    Move to lib/

---
+++ lib/smooth-scrollbar.ts
@@ -0,0 +1,125 @@
+import React, {
+  createElement,
+  cloneElement,
+  forwardRef,
+  isValidElement,
+  useEffect,
+  useCallback,
+  useRef,
+} from 'react';
+import SmoothScrollbar from 'smooth-scrollbar';
+import type {Scrollbar} from 'smooth-scrollbar/scrollbar';
+import type {ScrollStatus} from 'smooth-scrollbar/interfaces';
+
+import {ScrollbarProps} from './types';
+
+const SmoothScrollbarReact = forwardRef<Scrollbar, ScrollbarProps>(
+  function SmoothScrollbarReact(
+    {children, className, style, ...restProps},
+    ref
+  ) {
+    const mountedRef = useRef(true);
+
+    const scrollbar = useRef<Scrollbar>(null!);
+
+    const handleScroll = useCallback(
+      (status: ScrollStatus) => {
+        if (typeof restProps.onScroll === 'function') {
+          restProps.onScroll(status, scrollbar.current);
+        }
+      },
+      [restProps.onScroll]
+    );
+
+    const containerRef = useCallback(async node => {
+      if (node instanceof HTMLElement) {
+        if (restProps.plugins?.overscroll) {
+          const {default: OverscrollPlugin} = await import(
+            'smooth-scrollbar/plugins/overscroll'
+          );
+          SmoothScrollbar.use(OverscrollPlugin);
+        }
+
+        scrollbar.current = SmoothScrollbar.init(node, restProps);
+        scrollbar.current.addListener(handleScroll);
+
+        if (ref) {
+          (ref as React.MutableRefObject<Scrollbar>).current =
+            scrollbar.current;
+        }
+      }
+    }, []);
+
+    useEffect(() => {
+      return () => {
+        scrollbar.current.removeListener(handleScroll);
+        scrollbar.current.destroy();
+      };
+    }, []);
+
+    useEffect(() => {
+      if (mountedRef.current) {
+        mountedRef.current = false;
+      } else {
+        if (scrollbar.current) {
+          Object.keys(restProps).forEach(key => {
+            if (!(key in scrollbar.current.options)) {
+              return;
+            }
+
+            if (key === 'plugins') {
+              Object.keys(restProps.plugins).forEach(pluginName => {
+                scrollbar.current.updatePluginOptions(
+                  pluginName,
+                  restProps.plugins[pluginName]
+                );
+              });
+            } else {
+              // @ts-ignore
+              scrollbar.current.options[key] = restProps[key];
+            }
+          });
+
+          scrollbar.current.update();
+        }
+      }
+    }, [restProps]);
+
+    if (isValidElement(children)) {
+      return cloneElement(children, {
+        ref: containerRef,
+        className:
+          (children.props.className ? `${children.props.className} ` : '') +
+          className,
+        style: {
+          ...style,
+          ...children.props.style,
+        },
+      });
+    }
+
+    return createElement(
+      'div',
+      {
+        ref: containerRef,
+        className,
+        style: {
+          ...style,
+          WebkitBoxFlex: 1,
+          msFlex: 1,
+          MozFlex: 1,
+          flex: 1,
+        },
+      },
+      createElement(
+        'div',
+        {
+          className,
+        },
+        children
+      )
+    );
+  }
+);
+
+export {SmoothScrollbarReact as Scrollbar};
---
+++ lib/types.ts
@@ -0,0 +1,20 @@
+import type {Scrollbar} from 'smooth-scrollbar/scrollbar';
+import type {ScrollbarOptions, ScrollStatus} from 'smooth-scrollbar/interfaces';
+import type {
+  OverscrollOptions,
+  OverscrollEffect,
+} from 'smooth-scrollbar/plugins/overscroll';
+
+export interface ScrollbarPlugin extends Record<string, unknown> {
+  overscroll?: Partial<Omit<OverscrollOptions, 'effect'>> & {
+    effect?: OverscrollEffect;
+  };
+}
+
+export type ScrollbarProps = Partial<ScrollbarOptions> &
+  React.PropsWithChildren<{
+    className?: string;
+    style?: React.CSSProperties;
+    plugins?: ScrollbarPlugin;
+    onScroll?: (status: ScrollStatus, scrollbar: Scrollbar | null) => void;
+  }>;
